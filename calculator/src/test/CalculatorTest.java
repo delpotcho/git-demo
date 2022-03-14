65ef7175c8896e3cf9f70397bfdf54813771a44e
Nour Mohammed <nourdev01@gmail.com>
2022-03-14 17:19:43 +0100

    [feat]CALC-1

---
+++ calculator/src/test/CalculatorTest.java
@@ -0,0 +1,17 @@
+package test;
+
+public class CalculatorTest {
+    public  Integer addition(int x , int y){
+        return x+y;
+
+    }
+    public Integer Subtraching (Integer x , Integer y){
+   return x/y;
+    }
+    public Integer multiplicztion (Integer x , Integer y){
+        return x*y;
+    }
+    public Integer division (Integer x , Integer y){
+        return x/y;
+    }
+}
