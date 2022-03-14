7af0bd417ac6dc7c0b0d88be567739efc89a4c8e
Nour Mohammed <nourdev01@gmail.com>
2022-03-14 18:01:31 +0100

    add commantaire

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -1,3 +1,18 @@
 public class Calculator {
+    public  Integer addition(int x , int y){
+        return x+y;
+
+    }
+    //substraction
+    public Integer Subtraching (Integer x , Integer y){
+        return x/y;
+    }
+    //multiplication
+    public Integer multiplicztion (Integer x , Integer y){
+        return x*y;
+    }
+    public Integer division (Integer x , Integer y){
+        return x/y;
+    }
 
 }
